7decb81510bdccf5ab3ca02778e4ec1164e45e1a
huangwantong <huangwantong@escopetech.com>
2022-04-18 11:58:38 +0800

    fix: 修改最新k线图地址

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -73,7 +73,7 @@
 import echarts from "echarts"
 import pako from 'pako'
 import utils from '../utils/utils.js'
-import {colors} from '../config/index'
+import { colors, huobiWsUrl } from '../config/index'
 export default {
   data() {
     return {
@@ -101,7 +101,7 @@ export default {
         interval: null,
         flag: false,
          // K 线相关
-        haurl:'wss://api.huobiasia.vip/ws',
+        haurl: huobiWsUrl,
         requestK: {req: 'market.eosusdt.kline.1min'},
         subK:{sub: 'market.eosusdt.kline.1min'},
         socketK:{},
